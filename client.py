6a802a01c305e394bae95701d40e8bb717469305
Ubuntu <ubuntu@ip-172-31-18-70.ec2.internal>
2023-07-28 17:15:21 +0000

    a

--- client.py
+++ client.py
@@ -16,20 +16,29 @@ def iter_return_file(byt: bytes):
 def run():
     with open("tree.jpg", "rb") as f:
         byt = f.read()
-    with grpc.insecure_channel('localhost:50051') as channel:
+    with grpc.insecure_channel('172.31.27.226:50051') as channel:
         stub = image_server_pb2_grpc.ProcessorStub(channel)
         time_ls = []
         for i in range(1000):
             start_g = time.time()
             iter = stub.GrayscaleImage(iter_return_file(byt))
+            arr = bytearray()
+            for resp in iter:
+                arr.extend(resp.image)
             end_g = time.time()
             
             start_i = time.time()
             iter = stub.InvertImage(iter_return_file(byt))
+            arr = bytearray()
+            for resp in iter:
+                arr.extend(resp.image)
             end_i = time.time()
             
             start_ic = time.time()
             iter = stub.IconImage(iter_return_file(byt))
+            arr = bytearray()
+            for resp in iter:
+                arr.extend(resp.image)
             end_ic = time.time()
 
             time_ls.append([str(i), end_g - start_g, end_i - start_i, end_ic - start_ic])
